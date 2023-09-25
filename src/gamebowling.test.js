fb05bf64b01ba8765229d71a2aa8999781a50f0f
wilnermena <wi_an9@hotmail.com>
2023-09-24 22:23:37 -0400

    Para el número 20

--- src/gamebowling.test.js
+++ src/gamebowling.test.js
@@ -2,13 +2,28 @@
 
 describe("JUEGO BOWLING", () => {
   it("Dato vacio", () => {
-    const escore=new game;
-    expect(escore.numero()).toEqual(0);
+    const escore=new game();
+    expect(escore.getScore()).toEqual(0);
   });
+  it("Para el numero 20", () => {
+    const escore=new game();
+    for(let i=0;i<20;i++){
+      escore.roll(1);
+    }
+    expect(escore.getScore()).toEqual(20);
+  });
+
 });
 
 class game{
-  numero(){
-    return 0
+  constructor(){
+    this.score=0;
+  }
+  roll(pins){
+    this.score +=pins;
+    
+  }
+  getScore(){
+    return this.score;
   }
 }
